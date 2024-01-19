89445e546afb9ca2cb4f3f6557149d8f4d7c7b7d
Wilson Gonçalves <62620096+WRSG-Goncalves@users.noreply.github.com>
2024-01-19 10:25:04 -0300

    InMemoryRepository crud test

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Entity } from '../../../entities/entity'
 
 import { InMemoryRepository } from '../../in-memory.repository'
+import { NotFoundError } from '../../../errors/not-found-error'
 
 type StubEntityProps = {
   name: string
@@ -22,5 +23,56 @@ describe('InMemoryRepository unit tests', () => {
     const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
     await sut.insert(entity)
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+
+  it('Should throw error when entity not found', async () => {
+    await expect(sut.findById('fakeId')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should find a entity by id', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
+    await sut.insert(entity)
+    const result = await sut.findById(entity._id)
+    expect(entity.toJSON()).toStrictEqual(result.toJSON())
+  })
+
+  it('Should returns all entities', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
+    await sut.insert(entity)
+    const result = await sut.findAll()
+    expect([entity]).toStrictEqual(result)
+  })
+
+  it('Should throw error on update when entity not found', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
+    await expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should update an entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
+    await sut.insert(entity)
+    const entityUpdated = new StubEntity(
+      { name: 'updated', price: 10 },
+      entity._id,
+    )
+    await sut.update(entityUpdated)
+    expect(entityUpdated.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+
+  it('Should throw error when entity not found', async () => {
+    await expect(sut.delete('fakeId')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should delete an entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 }, undefined)
+    await sut.insert(entity)
+    await sut.delete(entity._id)
+    expect(sut.items).toHaveLength(0)
   })
 })
