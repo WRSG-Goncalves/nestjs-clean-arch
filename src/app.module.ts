db28f02cc869b6b9470cc8da8bc4bef397fad581
Wilson Gonçalves <62620096+WRSG-Goncalves@users.noreply.github.com>
2024-01-24 17:21:47 -0300

    Casos de uso e testes unitários

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
+//import { UsersModule } from './users/infrastructure/users.module'
+//import { DatabaseModule } from './shared/infrastructure/database/database.module'
+import { AuthModule } from './auth/infrastructure/auth.module'
+
+@Module({
+  //imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
+  imports: [EnvConfigModule, AuthModule],
+})
+export class AppModule {}
