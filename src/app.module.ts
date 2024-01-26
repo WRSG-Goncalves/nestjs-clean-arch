11aff3fe272f0219622bde5694d53e9731f9a7d6
Wilson Gonçalves <62620096+WRSG-Goncalves@users.noreply.github.com>
2024-01-26 15:59:24 -0300

    Adding prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
-//import { UsersModule } from './users/infrastructure/users.module'
-//import { DatabaseModule } from './shared/infrastructure/database/database.module'
+import { UsersModule } from './users/infrastructure/users.module'
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { AuthModule } from './auth/infrastructure/auth.module'
 
 @Module({
-  //imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
-  imports: [EnvConfigModule, AuthModule],
+  imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
 })
 export class AppModule {}
